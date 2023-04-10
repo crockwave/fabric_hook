b563434be2343f1d8e8485d99f03996cace5a101
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2023-04-10 12:54:25 +0530

    Update mix.exs
    added npm install to mix setup

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule FabricHook.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+       setup: ["deps.get", "ecto.setup", "cmd --cd assets npm install"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
